9f4e7c92ed67dbc0e7e3794ce51bc94c9df230f1
Harsh <harshj200230@gmail.com>
2023-09-24 16:12:19 +0530

    login implemnetation-1

--- app/src/main/java/com/example/minor_project/login.java
+++ app/src/main/java/com/example/minor_project/login.java
@@ -1,45 +1,91 @@
 package com.example.minor_project;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.example.minor_project.databinding.LoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class login extends AppCompatActivity {
 
-    private EditText usernameEditText;
-    private EditText passwordEditText;
-    private Button loginButton;
+    private LoginBinding binding;
 
-    @SuppressLint("MissingInflatedId")
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = LoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        usernameEditText = findViewById(R.id.usernameEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-        loginButton = findViewById(R.id.loginButton);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        binding.googlesigninBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-
-                // Here, you can add your login logic.
-                // For this example, let's just check if both fields are filled.
-                if (!username.isEmpty() && !password.isEmpty()) {
-                    // Successful login
-                    Toast.makeText(login.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Invalid input
-                    Toast.makeText(login.this, "Please enter valid credentials.", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+                signIn();
             }
         });
+
+
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Intent intent = new Intent(login.this, MainActivity.class);
+                startActivity(intent);
+            } catch (ApiException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+                Log.d("ERROR",e.toString());
+
+            }
+
+        }
+    }
+
+    private void chekforDomain(GoogleSignInAccount account) {
+        String email = account.getEmail();
+
+        // Check if the email address contains the specified domain
+        if (email != null && email.endsWith("@mail.jiit.ac.in")) {
+            Toast.makeText(this, "Sign-in successful!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(login.this, MainActivity.class);
+            startActivity(intent);
+
+        } else {
+
+            Toast.makeText(this, "Sign-in failed: Invalid domain!", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
