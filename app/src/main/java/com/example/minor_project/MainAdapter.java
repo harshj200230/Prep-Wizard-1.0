41f90b65b36cb3fa9db3eebcb538dabe2f0ddd08
Harsh <harshj200230@gmail.com>
2023-11-18 22:33:54 +0530

    data show hora list me

--- app/src/main/java/com/example/minor_project/MainAdapter.java
+++ app/src/main/java/com/example/minor_project/MainAdapter.java
@@ -2,38 +2,67 @@ package com.example.minor_project;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.File;
+import com.example.minor_project.R;
+import com.example.minor_project.pdfClass;
+
 import java.util.List;
 
-public class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
 
     private Context context;
-    private List<File> pdfFiles;
+    private List<pdfClass> pdfFiles;
+    private OnItemClickListener onItemClickListener;
 
-    public MainAdapter(Context context) {
+    public MainAdapter(Context context, List<pdfClass> pdfFiles, OnItemClickListener onItemClickListener) {
         this.context = context;
+        this.pdfFiles = pdfFiles;
+        this.onItemClickListener = onItemClickListener;
     }
 
     @NonNull
     @Override
     public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MainViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_item,parent),false);
+        View view = LayoutInflater.from(context).inflate(R.layout.rv_item, parent, false);
+        return new MainViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
-        holder.txtName.setText(pdfFiles.get(position).getName());
+        pdfClass pdfFile = pdfFiles.get(position);
+        holder.txtName.setText(pdfFile.getName());
         holder.txtName.setSelected(true);
 
+        // Set click listener on the item view
+        holder.itemView.setOnClickListener(view -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(pdfFile);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return pdfFiles.size();
+    }
+
+    public class MainViewHolder extends RecyclerView.ViewHolder {
+        TextView txtName;
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.pdfTextName);
+        }
+    }
+
+    // Interface to handle item clicks
+    public interface OnItemClickListener {
+        void onItemClick(pdfClass pdfFile);
     }
 }
--- app/src/main/java/com/example/minor_project/PDFVIEW.java
+++ app/src/main/java/com/example/minor_project/PDFVIEW.java
@@ -1,24 +1,92 @@
 package com.example.minor_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-import java.io.File;
+import com.example.minor_project.databinding.ActivityPdfviewBinding;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class PDFVIEW extends AppCompatActivity {
+public class PDFVIEW extends AppCompatActivity implements MainAdapter.OnItemClickListener {
 
+    private ActivityPdfviewBinding binding;
+    private DatabaseReference databaseReference;
+    private List<pdfClass> uploads;
     private MainAdapter adapter;
-    private List<File> pdfList;
-    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdfview);
+        binding = ActivityPdfviewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        FirebaseApp.initializeApp(this);
+
+
+        uploads = new ArrayList<>();
+        adapter = new MainAdapter(this, uploads, this);
+
+        RecyclerView recyclerView = binding.rv;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        viewSampleFiles();
+
+        viewAllFiles();
     }
 
+    private void viewAllFiles() {
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                uploads.clear(); // Clear existing data before adding new data
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    pdfClass pdfClass = postSnapshot.getValue(pdfClass.class);
+                    uploads.add(pdfClass);
+                }
+
+                // Notify the adapter that the data has changed
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle onCancelled
+            }
+        });
+    }
+    private void viewSampleFiles() {
+        // Sample data for testing
+        uploads.clear();
+        uploads.add(new pdfClass("Sample PDF 1", "https://content.bridgepointeducation.com/curriculum/file/48d7a132-3143-4b60-a261-5d9dca5b88de/1/Sample%20Business%20Report.pdf"));
+        uploads.add(new pdfClass("Sample PDF 2", "https://firebasestorage.googleapis.com/v0/b/jiit-prepwizard.appspot.com/o/uploads%2FConstitutionPBL%5B1%5D.docx.pdf?alt=media&token=5140ac16-d54d-4266-a9db-08a49fce5270"));
+
+        // Notify the adapter that the data has changed
+        adapter.notifyDataSetChanged();
+    }
+
+
+    // Implement the onItemClick method from OnItemClickListener interface
+    @Override
+    public void onItemClick(pdfClass pdfFile) {
+        // Handle item click, e.g., open the PDF
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setType("application/pdf");
+        intent.setData(Uri.parse(pdfFile.getUrl()));
+        startActivity(intent);
+    }
 }
