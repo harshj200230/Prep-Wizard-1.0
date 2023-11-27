37d0fc52894be92afdf3ef206e9fa9576ebd8167
Harsh <harshj200230@gmail.com>
2023-11-27 14:53:12 +0530

    useractivity done

--- app/src/main/java/com/example/minor_project/user.java
+++ app/src/main/java/com/example/minor_project/user.java
@@ -1,14 +1,39 @@
 package com.example.minor_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.minor_project.databinding.ActivitySubjectBinding;
+import com.example.minor_project.databinding.ActivityUserBinding;
 
 public class user extends AppCompatActivity {
 
+    private ActivityUserBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        binding = ActivityUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user.this,login2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
