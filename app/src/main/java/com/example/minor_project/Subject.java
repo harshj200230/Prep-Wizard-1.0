2c5e1f513b206f55a2644df19056ba3661d9b304
Halcyon0143 <vatsakshat0143@gmail.com>
2023-12-20 12:23:05 +0530

    marquee text(not working)

--- app/src/main/java/com/example/minor_project/Subject.java
+++ app/src/main/java/com/example/minor_project/Subject.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.minor_project.databinding.ActivitySubjectBinding;
@@ -74,6 +75,11 @@ public class Subject extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Get the selected subject
                 String selectedSubject = subjects.get(position);
+
+                //Enable marquee
+                TextView marqueeText = findViewById(R.id.marqueeText);
+                marqueeText.setSelected(true);
+
 
                 // Get the selected term from the intent
                 Intent receivedIntent = getIntent();
