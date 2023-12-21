bd7658212a89dc6eb5c5c6e3c12804c46d902935
Harsh <harshj200230@gmail.com>
2023-12-21 21:03:35 +0530

    remote config added

--- app/src/main/java/com/example/minor_project/user.java
+++ app/src/main/java/com/example/minor_project/user.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
 
 import com.example.minor_project.databinding.ActivitySubjectBinding;
 import com.example.minor_project.databinding.ActivityUserBinding;
@@ -16,6 +22,7 @@ import com.example.minor_project.databinding.ActivityUserBinding;
 public class user extends AppCompatActivity {
 
     private ActivityUserBinding binding;
+    FirebaseRemoteConfig firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,13 @@ public class user extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityUserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)  // Fetch every hour, adjust as needed
+                .build();
+        firebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+
+        fetchLatestVersionFromFirebase();
 
         binding.student.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -131,6 +145,38 @@ public class user extends AppCompatActivity {
 
 
 
+    }
+
+    private void fetchLatestVersionFromFirebase() {
+        firebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                String latestVersion = firebaseRemoteConfig.getString("latest_app_version");
+//                Toast.makeText(this, "latest version is : "+ latestVersion, Toast.LENGTH_SHORT).show();
+                String currentVersion = getAppVersionName();  // Get the current version of your app
+
+                if (!latestVersion.equals(currentVersion)) {
+                    // Notify the user that a newer version is available.
+                    Toast.makeText(user.this, "A newer version is available. Please update.", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(user.this, "Failed to fetch remote config.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private String getAppVersionName() {
+        try {
+            PackageManager packageManager = getApplicationContext().getPackageManager();
+            CharSequence appName = packageManager.getApplicationLabel(packageManager.getApplicationInfo(getPackageName(), 0));
+
+            // Now, 'appName' contains the name of your current app
+            String appNameString = appName.toString();
+            Toast.makeText(this, "version is : "+ appNameString, Toast.LENGTH_SHORT).show();
+            return appNameString;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
     private void openGoogleForm() {
